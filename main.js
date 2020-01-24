1483f035f9098e507c3fc5515207c4044ef2da54
Sofiya Lazarova <sofiya@sofiya.ca>
2020-01-24 14:57:26 +0100

    added all necessary functionality

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ function createTimeString() {
 
 function writeBotMessage(botMessage) {
 	let timeString = createTimeString();
-	$("#chat-history").append("<div class=\"bot-message\">" 
+	$("#chat-history").append("<div class=\"message\" id=\"bot-message\">" 
 		+ botMessage + "<div class=\"bot-time\">" 
 		+ timeString + "</div>" +"</div>");
 };
 
 function writeUserMessage(userMessage) {
 	let timeString = createTimeString();
-	$("#chat-history").append("<div class=\"user-message\">" + userMessage + "<div class=\"user-time\">" 
+	$("#chat-history").append("<div class=\"message\" id=\"user-message\">" + userMessage + "<div class=\"user-time\">" 
 		+ timeString + "</div>" + "</div>");
 };
 
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	const botInitialScript = ["Hello, I'm Bloop!", "I'm lonely, want to chat?"];
 	const botScript = ["Tell me about yourself.", "That's interesting!", "What are your favourite hobbies?",
 	"Really? I like browsing Reddit too!", "You're a really interesting human. But I'm getting tired. Bye now!", 
-	"I said I'm tired!", "Quit it.", "Okay, fine I'll tell you a story. Do you like fairies?", "Ugh okay, bye!"];
+	"I said I'm tired!", "Quit it.", "Okay, fine I'll tell you a story. Do you like fairies?", "No? Okay, bye!"];
 
 	for (let i = 0; i < botInitialScript.length; i++) {
 		writeBotMessage(botInitialScript[i]);
@@ -33,29 +33,27 @@ $(document).ready(function() {
 	let botCounter = 0;
 
 	$("#send-message").click(function(){
+		clearTimeout(timeout);
+
 		if (!$.trim($("textarea").val())) {
 			alert("Please send a nonempty message!");
 		}
 		else {
-			let userMessage = $("textarea").val();
+		    let userMessage = $("textarea").val();
 			writeUserMessage(userMessage);
 			$("textarea").val("");
-			clearTimeout(timeout);
 			if (botCounter < 9) {
 				writeBotMessage(botScript[botCounter]);
+				$("#chat-history").animate({scrollTop: $("#chat-history").prop("scrollHeight")}, 'slow');
 				botCounter++;
 			}
 			else {
 				writeBotMessage("Bloop has left the chat.");
+				$("#chat-history").animate({scrollTop: $("#chat-history").prop("scrollHeight")}, 'slow');
 			}	
 		}
 	}); 
 });
-
-
-
-
-
 
 
 
